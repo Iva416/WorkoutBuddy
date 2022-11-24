ea17be1ac3bf5047dce048a2caf5d695d2072a24
puentedg <puentedg@gmail.com>
2022-11-24 11:28:55 -0500

    exercise model

---
+++ server/models/Exercises.js
@@ -0,0 +1,22 @@
+const { Exercise, model } = require('mongoose');
+
+const exerciseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  start: {
+    type: String,
+  },
+  time : { 
+    type : Date, 
+    default: Date.now },
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
