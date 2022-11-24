b204545ef0668c9c57a6e8701a97c8a1b174e43c
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-11-24 10:46:08 -0500

    Update Files

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const index = require('./index');
+const Food = require('./Food');
+const Workout = require('./Workout');
+
+module.exports = { User, Workout, Food, index };
