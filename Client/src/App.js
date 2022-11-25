4ab1e95be03b8997d42b815180eecb7e4fb517fe
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-11-25 11:20:09 -0500

    changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
@@ -10,10 +10,10 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// import Home from './components/pages/Home';
-// import Login from './components/pages/Login';
-// import Calendar from './components/pages/Calendar';
-// import Food from './components/pages/Food';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Calendar from './components/pages/Calendar';
+import Food from './components/pages/Food';
 import Workout from './components/pages/Workout';
 import HomeContainer from './components/HomeContainer';
 import NavTabs from './components/NavTabs';
@@ -28,12 +28,12 @@ function App() {
               <HomeContainer>
                 <NavTabs />
                 <Routes>
-                  {/* <Route path="/home" element={<Home />} />
+                  <Route path="/home" element={<Home />} />
                   <Route path="/login" element={<Login />} />
-                  <Route path="/calendar" element={<Calendar />} /> */}
+                  <Route path="/calendar" element={<Calendar />} />
                   <Route path="/workout" element={<Workout />} />
-                  {/* <Route path="/food/:id" element={<Food />} /> */}
-                  {/* <Route path="*" element={<NoMatch />} /> */}
+                  <Route path="/food/:id" element={<Food />} />
+                  <Route path="*" element={<NoMatch />} />
                 </Routes>
               </HomeContainer>
             </div>
