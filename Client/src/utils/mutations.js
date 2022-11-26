a915d1e49b705c20a4796df72a23f95d9de57a3f
puentedg <puentedg@gmail.com>
2022-11-26 10:43:08 -0500

    mutation

--- Client/src/utils/mutations.js
+++ Client/src/utils/mutations.js
@@ -1,10 +1,10 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
       token
-      profile {
+      User {
         _id
         name
       }
@@ -13,11 +13,15 @@ export const ADD_PROFILE = gql`
 `;
 
 export const ADD_EXERCISE = gql`
-  mutation addExercise($profileId: ID!, $exercise: String!) {
-    addExercise(profileId: $profileId, exercise: $exercise) {
-      _id
-      name
-      skills
+  mutation addExercise($userId: ID!, $exercise: String!) {
+    addExercise(userId: $userId, exercise: $exercise) {
+        exercises {
+            exercise_name
+            source
+          }
+          muscle_group
+          time
+          description
     }
   }
 `;
@@ -26,7 +30,7 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
         name
       }
