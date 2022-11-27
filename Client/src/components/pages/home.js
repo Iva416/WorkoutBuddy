0a0bdc1f391764d7e33d8a64a558b4d685dc49e4
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-11-27 15:26:44 -0500

    Updated Package

--- Client/src/components/pages/home.js
+++ Client/src/components/pages/home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import "../../styles/WorkoutPage.css"; 
+import '../../styles/WorkoutPage.css';
 
 export default function Home() {
   return (
     <div>
       <h1 id="workout-header">WORKOUT BUDDY</h1>
-      <p id="motivation-note">
-        HERE FOR YOU ANYTIME!
-      </p>
+      <p id="motivation-note">HERE FOR YOU ANYTIME!</p>
     </div>
   );
 }
